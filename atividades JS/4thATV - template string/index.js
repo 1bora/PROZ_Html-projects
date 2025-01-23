ffc498628379194874e9d2e8ccf6831ed270af17
Athos Reis <athos3655@gmail.com>
2025-01-23 17:45:29 -0300

    template string

---
+++ atividades JS/4thATV - template string/index.js
@@ -0,0 +1,11 @@
+const ops = (n1,n2) => {
+  console.log(`Números para operação: ${n1} ${n2}`);
+  console.log(`OP -> Soma: ${n1} + ${n2} = ${n1+n2}`);
+  console.log(`OP -> Subtração: ${n1} - ${n2} = ${n1-n2}`);
+  console.log(`OP -> Multiplicação: ${n1} x ${n2} = ${n1*n2}`);
+  console.log(`OP -> Divisão: ${n1} / ${n2} = ${n1/n2}`);
+
+}
+
+
+ops(1,5);
